d260253ebfc2d9ff8d48e94aa4ee9e193875578b
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-02-23 18:25:06 +0800

    second exercise

---
+++ creative-assignments/spell.js
@@ -0,0 +1,16 @@
+console.log("second exercise") 
+
+  let str = "";
+
+function solution(str){
+
+  str = "world";
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] = str) {
+      console.log(str[i]);
+    }
+  }
+  
+  
+}
+solution(str)
