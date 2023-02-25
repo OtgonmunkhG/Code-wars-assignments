5a20108254941d38172189949fc4f8d33a7b7f3a
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-02-25 21:23:47 +0800

    OOP

---
+++ creative-assignments/object.oriented.js
@@ -0,0 +1,23 @@
+class Person {
+  constructor() {
+    this.firstName = "John";
+    this.lastName = "Doe";
+    this.age = 0;
+    this.gender = "Male";
+  }
+  
+
+  
+  // Get coding in ES6 :D
+}
+class sayFullName {
+  
+    // return this.firstName , this.lastName;
+    
+  }
+  
+  class greetExtraTerrestrials {
+    constructor(raceName) {
+      
+    }
+  }
