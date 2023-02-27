2901e563b731446131f32608bfe334fd22fcd997
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-02-27 21:30:15 +0800

    distance

---
+++ creative-assignments/distance.js
@@ -0,0 +1,13 @@
+class point {
+  construction(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+function distanceBetweenPoints(a, b) {
+  
+  let distance = 0;
+  distance = a - b;
+  return distance; // your code here
+}
